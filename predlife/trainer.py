09552b02daaff425caaa441088ffc7f733ace495
Shoji Ihara <shoji.ihara@gmail.com>
2014-10-23 04:53:14 +0900

    added retrainer

--- predlife/trainer.py
+++ predlife/trainer.py
@@ -5,11 +5,21 @@ from pybrain.datasets.supervised import SupervisedDataSet
 from pybrain.structure import TanhLayer
 from pybrain.tools.shortcuts import buildNetwork
 
-def train(dataset):
+def train(N, dataset):
     ds = SupervisedDataSet(20, 20)
     for data in dataset:
         ds.addSample(data[0], data[1])
     net = buildNetwork(20, 30, 10, 20, bias=True, hiddenclass=TanhLayer)
     trainer = BackpropTrainer(net, ds)
-    trainer.train()
-    return trainer
+    for i in range(N):
+        trainer.train()
+    return net
+
+def retrain(N, dataset, net):
+    ds = SupervisedDataSet(20, 20)
+    for data in dataset:
+        ds.addSample(data[0], data[1])
+    trainer = BackpropTrainer(net, ds)
+    for i in range(N):
+        trainer.train()
+    return net
