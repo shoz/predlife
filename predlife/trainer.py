f6209a56773af423ea3e97f8a1e5ea9d51b4f644
Shoji Ihara <shoji.ihara@gmail.com>
2014-10-23 11:53:27 +0900

    fixed the number of layers

--- predlife/trainer.py
+++ predlife/trainer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets.supervised import SupervisedDataSet
 from pybrain.structure import TanhLayer
@@ -9,9 +10,11 @@ def train(N, dataset):
     ds = SupervisedDataSet(20, 20)
     for data in dataset:
         ds.addSample(data[0], data[1])
-    net = buildNetwork(20, 30, 10, 20, bias=True, hiddenclass=TanhLayer)
+    net = buildNetwork(20, 20, 20, bias=True, hiddenclass=TanhLayer)
     trainer = BackpropTrainer(net, ds)
     for i in range(N):
+        sys.stdout.write("Progress: %d/%d \r" % (i, N))
+        sys.stdout.flush()
         trainer.train()
     return net
 
